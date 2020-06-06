b7ba999618c200968443b5023c5ae2ddeaf2bc69
Bernardo <krunz04@gmail.com>
2020-06-06 10:01:22 +0200

    remove

--- Task6/evaluateLikelihoodAOA.m
+++
@@ -1,9 +0,0 @@
-function likelihood = evaluateLikelihoodAOA(parameters,rho,AP,evaluationPoint)
-
-evaluationRho = atan2((evaluationPoint(2)-AP(2)),(evaluationPoint(1)-AP(1))); 
-            
-argument = rho - evaluationRho;
-
-likelihood = 1/sqrt(2*pi*parameters.sigmaAOA.^2)*exp(-0.5* argument^2 / parameters.sigmaAOA.^2);
-
-end
